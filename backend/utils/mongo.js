0e14260a6cd78c175a91bb4d816a1547e4e670de
james <jamesliwood@gmail.com>
2025-02-19 05:18:21 -0500

    added a validateUser function

--- backend/utils/mongo.js
+++ backend/utils/mongo.js
@@ -1,4 +1,5 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const bcrypt = require("bcrypt");
 
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri, {
@@ -18,6 +19,20 @@ async function connectToMongo() {
   }
 }
 
+async function validateUser(username, password) {
+  const db = client.db("restaurant");
+  const collection = db.collection("employees");
+
+  const result = await collection.find({ username: username }).toArray();
+  if (result.length == 0) return false;
+
+  const verifyPassword = await bcrypt.compare(password, result[0].password);
+  if (!verifyPassword) return false;
+
+  return result[0];
+}
+
 module.exports = {
   connectToMongo: connectToMongo,
+  validateUser: validateUser,
 };
