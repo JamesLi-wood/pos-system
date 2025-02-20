f0b3a541fbabea8b7976bb06abb2b930e0d760a9
james <jamesliwood@gmail.com>
2025-02-20 01:14:35 -0500

    added table component

---
+++ frontend/app/table/table.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { useState } from "react";
+import Order from "./order";
+const Table = ({
+  menu,
+  tables,
+}: {
+  menu: Array<object>;
+  tables: Array<string>;
+}) => {
+  const [inventory, setInventory] = useState(false);
+  const [tableName, setTableName] = useState("");
+
+  const activateInventory = async (table: string) => {
+    setInventory(true);
+    setTableName(table);
+  };
+
+  const RenderTables = () => {
+    return (
+      <div className="flex justify-between h-full">
+        <div>
+          {tables.map((table) => {
+            return (
+              <div
+                id={table}
+                key={table}
+                className="h-[100px] w-[100px] border border-red-500"
+                onClick={() => {
+                  activateInventory(table);
+                }}
+              >
+                {`Table ${table}`}
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="w-60 border border-red-500 overflow-y-scroll scroll-hidden">
+          <div className="h-[2000px]">TAKEOUT LOGIC GOES HERE</div>
+          <div>BOTTOM CONTENT</div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      {!inventory ? (
+        <RenderTables />
+      ) : (
+        <Order menu={menu} tableName={tableName} setInventory={setInventory} />
+      )}
+    </>
+  );
+};
+
+export default Table;
