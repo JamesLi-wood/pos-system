319e6d12159b75d5b930fb08dc080d418bac2e69
james <jamesliwood@gmail.com>
2025-02-20 00:38:31 -0500

    updated logic where users can still be in the page even though the token expired

--- frontend/app/components/validateToken.tsx
+++ frontend/app/components/validateToken.tsx
@@ -5,13 +5,23 @@ import { redirect } from "next/navigation";
 const ValidateToken = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
     const token = localStorage.getItem("token");
+    const userLog = sessionStorage.getItem("userConnected");
+
     if (!token) redirect("/auth");
 
     const payload = JSON.parse(atob(token.split(".")[1]));
-    const expirationDate = payload.exp * 1000;
-    if (Date.now() > expirationDate) {
+    const hasTokenExpired = Date.now() > payload.exp * 1000;
+
+    /* Continues session as long as the user's token is valid
+    or is currently connected. */
+    if (hasTokenExpired && !userLog) {
       localStorage.removeItem("token");
       redirect("/auth");
+    }
+
+    // Will connect the user as long as token hasn't expired.
+    if (!userLog) {
+      sessionStorage.setItem("userConnected", "true");
     }
   }, []);
 
