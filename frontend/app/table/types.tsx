51b18b203f5ac6f6da78378733b608718f3e4aa4
james <jamesliwood@gmail.com>
2025-02-21 23:00:27 -0500

    added file for explicit types

---
+++ frontend/app/table/types.tsx
@@ -0,0 +1,42 @@
+export interface SingularOptionsData {
+  title: string;
+  choices: Array<string>;
+}
+
+export interface MultipleOptionsData {
+  title: string;
+  choices: {
+    title: string;
+    options: Array<string>;
+  };
+}
+
+export interface MenuItemData {
+  _id: string;
+  name: string;
+  price: number;
+  description: string;
+  singularOptions: SingularOptionsData[];
+  multipleOptions: MultipleOptionsData[];
+}
+
+export interface MenuData {
+  name: string;
+  data: MenuItemData[];
+}
+
+export interface OrderItem {
+  id: number;
+  name: string;
+}
+
+export interface TableContextType {
+  menu: MenuData[];
+  tableName: string;
+  setInventory: Function;
+  removeSlidedownContent: () => void;
+  currentOrder: OrderItem[];
+  setCurrentOrder: React.Dispatch<React.SetStateAction<OrderItem[]>>;
+  currentPrice: number;
+  setCurrentPrice: React.Dispatch<React.SetStateAction<number>>;
+}
