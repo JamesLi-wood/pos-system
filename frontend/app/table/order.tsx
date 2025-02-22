7d44c9b3b431144f394c9c1304f6da51fcd8222e
james <jamesliwood@gmail.com>
2025-02-21 23:01:41 -0500

    updated order to showcase the menu and menu items

--- frontend/app/table/order.tsx
+++ frontend/app/table/order.tsx
@@ -1,26 +1,123 @@
 "use client";
+import React, { useState, createContext } from "react";
+import Ticket from "./ticket";
+import MenuItem from "./menuItem";
+import SlideDown from "../components/slidedown";
+import {
+  MenuData,
+  OrderItem,
+  TableContextType,
+} from "./types";
+
+export const tableContext = createContext<TableContextType | undefined>(
+  undefined
+);
 
 const Order = ({
   menu,
   tableName,
   setInventory,
 }: {
-  menu: Array<object>;
+  menu: MenuData[];
   tableName: string;
   setInventory: Function;
 }) => {
+  const [options, setOptions] = useState<MenuData>(menu[0]);
+  const [slidedownContent, setSlidedownContent] =
+    useState<React.ReactNode | null>(null);
+  const [selectedItem, setSelectedItem] = useState(0);
+  const [currentOrder, setCurrentOrder] = useState<OrderItem[]>([]);
+  const [currentPrice, setCurrentPrice] = useState(0);
+
+  const removeSlidedownContent = () => {
+    setSlidedownContent(null);
+  };
+
+  const contextValue = {
+    menu,
+    tableName,
+    setInventory,
+    removeSlidedownContent,
+    currentOrder,
+    setCurrentOrder,
+    currentPrice,
+    setCurrentPrice,
+  };
+
   return (
-    <div>
-      <div>ORDER</div>
-      <button
-        className="bg-red-500"
-        onClick={() => {
-          setInventory(false);
-        }}
+    <tableContext.Provider value={contextValue}>
+      <div
+        className={`bg-black text-white h-full ${
+          slidedownContent && "pointer-events-none"
+        }`}
       >
-        BACK
-      </button>
-    </div>
+        <div className="flex gap-4">
+          <div className="text-4xl">{tableName}</div>
+          <button
+            className="bg-red-500 p-2"
+            onClick={() => setInventory(false)}
+          >
+            Return
+          </button>
+          <button
+            className="bg-green-500 p-2"
+            onClick={() => {
+              setSlidedownContent(<Ticket />);
+            }}
+          >
+            View Ticket
+          </button>
+        </div>
+
+        <div className="border flex">
+          <div className="border px-2">
+            {menu.map((item, idx) => {
+              return (
+                <div
+                  key={item.name}
+                  className={`${
+                    selectedItem === idx && "bg-gray-800"
+                  } mb-2 cursor-pointer rounded-lg border-2 border-transparent p-2 hover:border-gray-300`}
+                  onClick={() => {
+                    setOptions(item);
+                    setSelectedItem(idx);
+                  }}
+                >
+                  {item.name}
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="w-full">
+            <div>{options.name}</div>
+            <div className="grid grid-cols-4">
+              {options.data.map((item) => {
+                return (
+                  <div
+                    key={item._id}
+                    className="border cursor-pointer"
+                    onClick={() => {
+                      setSlidedownContent(<MenuItem item={item} />);
+                    }}
+                  >
+                    <div>IMG</div>
+                    <div>{item.name}</div>
+                    <div>${item.price}</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+
+        {slidedownContent && (
+          <SlideDown handleClick={removeSlidedownContent}>
+            {slidedownContent}
+          </SlideDown>
+        )}
+      </div>
+    </tableContext.Provider>
   );
 };
 
