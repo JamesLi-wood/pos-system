b839e685f1a7f287f0e1079ccd114d9d5328eb58
james <jamesliwood@gmail.com>
2025-02-23 23:38:40 -0500

    turned ValidateToken into a hook

---
+++ frontend/app/hooks/useValidateToken.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
+
+const useValidateToken = () => {
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const userLog = sessionStorage.getItem("userConnected");
+
+    if (!token) redirect("/auth");
+
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    const hasTokenExpired = Date.now() > payload.exp * 1000;
+
+    /* Continues session as long as the user's token is valid
+    or is currently connected. */
+    if (hasTokenExpired && !userLog) {
+      localStorage.removeItem("token");
+      redirect("/auth");
+    }
+
+    // Will connect the user as long as token hasn't expired.
+    if (!userLog) {
+      sessionStorage.setItem("userConnected", "true");
+    }
+  }, []);
+};
+
+export default useValidateToken;
