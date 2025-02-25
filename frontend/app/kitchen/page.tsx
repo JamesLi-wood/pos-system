4417f52306462497b95e1e4ab037428de4882381
james <jamesliwood@gmail.com>
2025-02-25 00:52:12 -0500

    updated page to show kitchen tickets

--- frontend/app/kitchen/page.tsx
+++ frontend/app/kitchen/page.tsx
@@ -1,16 +1,50 @@
-"use client"
+"use client";
 import Link from "next/link";
+import { SetStateAction, useEffect, useState } from "react";
 import useValidateToken from "../hooks/useValidateToken";
+import { useSocket } from "../hooks/useSocket";
+import KitchenTicket from "./kitchenTicket";
 
+import { KitchenTicketType } from "../table/types";
 const Kitchen = () => {
   useValidateToken();
+  const [kitchenTickets, setKitchenTickets] = useState<KitchenTicketType[]>([]);
+  const socket = useSocket();
+
+  // Initial load.
+  useEffect(() => {
+    socket.emit("request-kitchen-ticket");
+  }, []);
+
+  // Listens to any changes on the ticket.
+  useEffect(() => {
+    const handleSocketRequest = (data: SetStateAction<KitchenTicketType[]>) => {
+      setKitchenTickets(data);
+    };
+
+    socket.on("get-kitchen-ticket", handleSocketRequest);
+
+    return () => {
+      socket.off("get-kitchen-ticket", handleSocketRequest);
+    };
+  }, [socket]);
 
   return (
-    <div>
-      <div>KITCHEN PAGE</div>
-      <Link className="bg-red-500" href={"/"}>
-        BACK
-      </Link>
+    <div className="h-full bg-black">
+      <div className="px-4 py-2">
+        <Link
+          href={"/"}
+          className="cursor-pointer border-2 border-white px-4 py-2 text-3xl text-white"
+        >
+          BACK
+        </Link>
+      </div>
+
+      <div className="max-[900px]:grid-cols-2 grid grid-cols-4 gap-4 bg-black p-4 text-xl">
+        {kitchenTickets.map((data) => {
+          return <KitchenTicket key={data.orderID} data={data} />;
+        })}
+      </div>
     </div>
   );
 };
