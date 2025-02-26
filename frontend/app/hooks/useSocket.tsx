2803c581a4cde350124ac72f8f93e2157d627061
james <jamesliwood@gmail.com>
2025-02-25 22:36:53 -0500

    user no longer gets an error message when they visit the link directly

--- frontend/app/hooks/useSocket.tsx
+++ frontend/app/hooks/useSocket.tsx
@@ -1,34 +1,16 @@
 "use client";
-import {
-  ReactNode,
-  useState,
-  useEffect,
-  createContext,
-  useContext,
-} from "react";
-import { io, Socket } from "socket.io-client";
+import { ReactNode, createContext, useContext } from "react";
+import { io } from "socket.io-client";
 
-const socketContext = createContext<Socket | null>(null);
+const socket = io(process.env.NEXT_PUBLIC_API_URL!);
+const socketContext = createContext(socket);
 
 export const SocketProvider = ({ children }: { children: ReactNode }) => {
-  const [socket, setSocket] = useState<Socket | null>(null);
-
-  useEffect(() => {
-    const socketInstance = io(process.env.NEXT_PUBLIC_API_URL!);
-    setSocket(socketInstance);
-
-    return () => {
-      socketInstance.disconnect();
-    };
-  }, []);
-
   return (
     <socketContext.Provider value={socket}>{children}</socketContext.Provider>
   );
 };
 
 export const useSocket = () => {
-  const socket = useContext(socketContext);
-  if (!socket) throw new Error("useSocket must be within a SocketProvider");
-  return socket;
+  return useContext(socketContext);
 };
