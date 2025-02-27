f356c01dc7bd2a5d3731c192d382a492409e0bbb
james <jamesliwood@gmail.com>
2025-02-27 03:23:57 -0500

    global types file

---
+++ frontend/app/types.tsx
@@ -0,0 +1,52 @@
+export interface SingularOptionsType {
+  title: string;
+  choices: Array<string>;
+}
+
+export interface MultipleOptionsType {
+  title: string;
+  choices: {
+    name: string;
+    price: number;
+  }[];
+}
+
+export interface MenuItemType {
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  singularOptions: SingularOptionsType[];
+  multipleOptions: MultipleOptionsType;
+}
+
+export interface MenuType {
+  name: string;
+  data: MenuItemType[];
+}
+
+export interface OrderType {
+  name: string;
+  price: number;
+  quantity: number;
+  singularOptions: Array<string>;
+  multipleOptions: Array<string>;
+  specialRequests: string;
+}
+
+export interface TableContextType {
+  menu: MenuType[];
+  tableName: string;
+  setInventory: Function;
+  removeSlidedownContent: () => void;
+  currentOrder: OrderType[];
+  setCurrentOrder: React.Dispatch<React.SetStateAction<OrderType[]>>;
+  currentPrice: number;
+  setCurrentPrice: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export interface TicketType {
+  dateCreated: number;
+  orderID: number;
+  ticket: OrderType[];
+}
