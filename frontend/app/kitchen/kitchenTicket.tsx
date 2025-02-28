34724997a8a1c2de8fc140d2026ec5b480ca4f61
james <jamesliwood@gmail.com>
2025-02-28 00:20:08 -0500

    removedTicket is now a prop

--- frontend/app/kitchen/kitchenTicket.tsx
+++ frontend/app/kitchen/kitchenTicket.tsx
@@ -1,16 +1,16 @@
 "use client";
 import { useMemo } from "react";
 import useTimer from "../hooks/useTimer";
-import { useSocket } from "../hooks/useSocket";
 import { TicketType } from "../types";
 
-const KitchenTicket = ({ data }: { data: TicketType }) => {
+const KitchenTicket = ({
+  data,
+  removeTicket,
+}: {
+  data: TicketType;
+  removeTicket: () => void;
+}) => {
   const [formatTime] = useTimer(data.dateCreated);
-  const socket = useSocket();
-
-  const remove = (id: number) => {
-    socket.emit("remove-kitchen-ticket", id);
-  };
 
   const ticketMap = useMemo(() => {
     return data.ticket.map((order, idx) => {
@@ -33,7 +33,7 @@ const KitchenTicket = ({ data }: { data: TicketType }) => {
 
       <button
         className="bg-[rgb(50,205,50)] cursor-pointer px-4 py-2 text-center text-white"
-        onClick={() => remove(data.orderID)}
+        onClick={removeTicket}
       >
         Press to complete
       </button>
