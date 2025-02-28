97dc9610ddd926582ba5e1e3725603c1cd94f517
james <jamesliwood@gmail.com>
2025-02-27 19:01:33 -0500

    ticket information will no longer rerender because of the useTimer hook

--- frontend/app/kitchen/kitchenTicket.tsx
+++ frontend/app/kitchen/kitchenTicket.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useMemo } from "react";
 import useTimer from "../hooks/useTimer";
 import { useSocket } from "../hooks/useSocket";
 import { TicketType } from "../types";
@@ -11,30 +12,31 @@ const KitchenTicket = ({ data }: { data: TicketType }) => {
     socket.emit("remove-kitchen-ticket", id);
   };
 
+  const ticketMap = useMemo(() => {
+    return data.ticket.map((order, idx) => {
+      return (
+        <div key={idx} className="px-4 py-1">
+          <p>{`${order.quantity} ${order.name}`}</p>
+        </div>
+      );
+    });
+  }, []);
+
   return (
     <div className="flex flex-col justify-between bg-white">
       <div className="bg-[rgb(50,205,50)] flex flex-row justify-between px-4 py-2 text-white">
-        <div>{`Order ${data.orderID}`}</div>
-        <div>{formatTime()}</div>
+        <p>{`Order ${data.orderID}`}</p>
+        <p>{formatTime()}</p>
       </div>
 
-      <div className="h-full py-2">
-        {data.ticket.map((order, idx) => {
-          return (
-            <div
-              key={idx}
-              className="px-4 py-1"
-            >{`${order.quantity} ${order.name}`}</div>
-          );
-        })}
-      </div>
+      <div className="h-full py-2">{ticketMap}</div>
 
-      <div
+      <button
         className="bg-[rgb(50,205,50)] cursor-pointer px-4 py-2 text-center text-white"
         onClick={() => remove(data.orderID)}
       >
         Press to complete
-      </div>
+      </button>
     </div>
   );
 };
