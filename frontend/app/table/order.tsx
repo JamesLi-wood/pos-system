50e3ec54fe0997249fd3649085cf2dd0da468c2d
james <jamesliwood@gmail.com>
2025-03-04 02:38:03 -0500

    menus no longer rerender when there is a slidedown content

--- frontend/app/table/order.tsx
+++ frontend/app/table/order.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useMemo } from "react";
 import SlideDown from "../components/slidedown";
 import Ticket from "./ticket";
 import MenuItem from "./menuItem";
@@ -40,10 +40,47 @@ const Order = ({
     setCurrentPrice,
   };
 
+  const handleMenuLoad = useMemo(() => {
+    return menu.map((item, idx) => {
+      return (
+        <p
+          key={item.name}
+          className={`${
+            selectedItem === idx && "bg-gray-800"
+          } cursor-pointer text-center rounded-lg p-2 border-transparent border-2 hover:border-gray-300`}
+          onClick={() => {
+            setOptions(item);
+            setSelectedItem(idx);
+          }}
+        >
+          {item.name}
+        </p>
+      );
+    });
+  }, [selectedItem]);
+
+  const handleOptionsLoad = useMemo(() => {
+    return options.data.map((item) => {
+      return (
+        <div
+          key={item._id}
+          className="border cursor-pointer h-40"
+          onClick={() => {
+            setSlidedownContent(<MenuItem item={item} />);
+          }}
+        >
+          <div>IMG</div>
+          <p>{item.name}</p>
+          <p>${item.price}</p>
+        </div>
+      );
+    });
+  }, [options]);
+
   return (
     <tableContext.Provider value={contextValue}>
       <div
-        className={`bg-black text-white h-full ${
+        className={`flex flex-col justify-between bg-black text-white h-full ${
           slidedownContent && "pointer-events-none"
         }`}
       >
@@ -65,45 +102,14 @@ const Order = ({
           </button>
         </div>
 
-        <div className="border flex">
-          <div className="border px-2">
-            {menu.map((item, idx) => {
-              return (
-                <p
-                  key={item.name}
-                  className={`${
-                    selectedItem === idx && "bg-gray-800"
-                  } mb-2 cursor-pointer rounded-lg border-2 border-transparent p-2 hover:border-gray-300`}
-                  onClick={() => {
-                    setOptions(item);
-                    setSelectedItem(idx);
-                  }}
-                >
-                  {item.name}
-                </p>
-              );
-            })}
+        <div className="flex flex-row h-[90%]">
+          <div className="overflow-y-scroll ml-4 mr-6 gap-2 flex flex-col flex-none">
+            {handleMenuLoad}
           </div>
 
-          <div className="w-full">
+          <div className="grow border border-red-500 overflow-y-scroll">
             <p>{options.name}</p>
-            <div className="grid grid-cols-4">
-              {options.data.map((item) => {
-                return (
-                  <div
-                    key={item._id}
-                    className="border cursor-pointer"
-                    onClick={() => {
-                      setSlidedownContent(<MenuItem item={item} />);
-                    }}
-                  >
-                    <div>IMG</div>
-                    <p>{item.name}</p>
-                    <p>${item.price}</p>
-                  </div>
-                );
-              })}
-            </div>
+            <div className="grid grid-cols-4">{handleOptionsLoad}</div>
           </div>
         </div>
 
