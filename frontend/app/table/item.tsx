d40dcc4465c24bc5faaf53c82a830cd2484a117f
james <jamesliwood@gmail.com>
2025-03-04 23:21:27 -0500

    removed the remove logic

--- frontend/app/table/item.tsx
+++ frontend/app/table/item.tsx
@@ -1,31 +1,14 @@
 import React, { useState } from "react";
-import { VscChromeClose } from "react-icons/vsc";
 import { SlArrowDown } from "react-icons/sl";
 import { OrderType } from "../types";
 
-const Item = ({
-  item,
-  handleRemove,
-  showDelete,
-}: {
-  item: OrderType;
-  handleRemove: () => void;
-  showDelete: Boolean;
-}) => {
+const Item = ({ item }: { item: OrderType }) => {
   const [showExtra, setShowExtra] = useState(false);
 
   return (
-    <div>
+    <div className="w-full">
       <div className="flex justify-between mt-2">
         <div className="flex items-center gap-2">
-          {showDelete && (
-            <button
-              className="w-7 flex items-center justify-center p-1 cursor-pointer border-none bg-[rgb(255,0,0)] text-white"
-              onClick={handleRemove}
-            >
-              <VscChromeClose />
-            </button>
-          )}
           <p>{`${item.quantity}x ${item.name}`}</p>
           <SlArrowDown
             onClick={() => setShowExtra((prevState) => !prevState)}
