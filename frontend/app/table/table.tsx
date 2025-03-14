214a0c830321e4c60d214e025f4b944c493ba5cd
james <jamesliwood@gmail.com>
2025-03-14 03:04:07 -0400

    updated props and shows a loading page for tables

--- frontend/app/table/table.tsx
+++ frontend/app/table/table.tsx
@@ -8,9 +8,11 @@ import { MenuType } from "../types";
 const Table = ({
   menu,
   tables,
+  loading,
 }: {
   menu: MenuType[];
   tables: Array<string>;
+  loading: boolean;
 }) => {
   const [inventory, setInventory] = useState(false);
   const [tableName, setTableName] = useState("");
@@ -22,28 +24,38 @@ const Table = ({
 
   const RenderTables = () => {
     return (
-      <div className="flex justify-between h-full">
-        <div>
-          {tables.map((table) => {
-            return (
-              <div
-                id={table}
-                key={table}
-                className="h-[100px] w-[100px] border border-red-500"
-                onClick={() => {
-                  activateInventory(table);
-                }}
-              >
-                {`Table ${table}`}
-              </div>
-            );
-          })}
-        </div>
+      <div className="flex justify-between h-full bg-black text-white">
+        {loading ? (
+          <div>LOADING</div>
+        ) : (
+          <div className="border mt-4 border-black flex-1 flex flex-col items-center gap-4">
+            {tables.map((table) => {
+              return (
+                <div
+                  id={table}
+                  key={table}
+                  className="h-[100px] w-[90%] rounded-xl border border-[rgb(211, 211, 211)] cursor-pointer justify-center items-center flex"
+                  onClick={() => {
+                    activateInventory(table);
+                  }}
+                >
+                  {`Table ${table}`}
+                </div>
+              );
+            })}
+          </div>
+        )}
 
-        <div className="w-60 border border-red-500 overflow-y-scroll scroll-hidden">
-          <Link className="bg-red-500" href={"/"}>
-            BACK
-          </Link>
+        <div className="w-60 border-l-4 border-[rgb(61,61,61)] overflow-y-scroll scroll-hidden">
+          <div className="flex w-full justify-center my-4">
+            <Link
+              className="w-[90%] rounded-xl bg-red-500 py-2 text-center"
+              href={"/"}
+            >
+              BACK
+            </Link>
+          </div>
+
           <Takeout
             activateInventory={() => {
               activateInventory("takeout");
