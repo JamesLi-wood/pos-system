f882d4def2610ea09da219843cf0cf429ebb2cfe
james <jamesliwood@gmail.com>
2025-03-20 01:30:37 -0400

    moved components into a seperate file

--- frontend/app/dashboard/menu/menuItems.tsx
+++ frontend/app/dashboard/menu/menuItems.tsx
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
 import { MenuItemType } from "@/app/types";
+import SlideDown from "@/app/components/slidedown";
+import MenuItemAdd from "./menuItemAdd";
+import MenuItemEdit from "./menuItemEdit";
 
 const MenuItems = ({ items }: { items: MenuItemType[] }) => {
   const [showEdit, setShowEdit] = useState(-1);
+  const [slideDownContent, setSlideDownContent] =
+    useState<React.ReactNode | null>(null);
 
   useEffect(() => {
     setShowEdit(-1);
@@ -12,18 +17,43 @@ const MenuItems = ({ items }: { items: MenuItemType[] }) => {
     <div>
       {items.map((item, idx) => {
         return (
-          <div
-            key={item.name}
-            className="flex"
-            onClick={() => {
-              showEdit === idx ? setShowEdit(-1) : setShowEdit(idx);
-            }}
-          >
-            <p>{item.name}</p>
-            {showEdit === idx && <button>Edit</button>}
+          <div key={item._id} className="flex">
+            <p
+              onClick={() => {
+                showEdit === idx ? setShowEdit(-1) : setShowEdit(idx);
+              }}
+            >
+              {item.name}
+            </p>
+            {showEdit === idx && (
+              <button
+                onClick={() => {
+                  setSlideDownContent(<MenuItemEdit />);
+                }}
+              >
+                Edit
+              </button>
+            )}
           </div>
         );
       })}
+      <button
+        onClick={() => {
+          setSlideDownContent(<MenuItemAdd />);
+        }}
+      >
+        Add
+      </button>
+
+      {slideDownContent && (
+        <SlideDown
+          handleRemove={() => {
+            setSlideDownContent(null);
+          }}
+        >
+          {slideDownContent}
+        </SlideDown>
+      )}
     </div>
   );
 };
