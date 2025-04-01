e2126a83a30b895d7022405bc881dddf71b1da32
james <jamesliwood@gmail.com>
2025-03-31 22:01:32 -0400

    added logic to apply changes

--- frontend/app/dashboard/menu/menuItemEdit.tsx
+++ frontend/app/dashboard/menu/menuItemEdit.tsx
@@ -1,28 +1,73 @@
-import { useRef, useEffect, useMemo } from "react";
+import { useRef, useEffect, useMemo, Dispatch, SetStateAction } from "react";
 import { MenuItemType } from "@/app/types";
 import useMenuItemConfig from "@/app/hooks/useMenuItemConfig";
 import InputGroups from "@/app/components/inputGroups";
 
-const MenuItemEdit = ({ item }: { item: MenuItemType }) => {
-  const mainEdit = useMenuItemConfig();
+const MenuItemEdit = ({
+  item,
+  sectionedMenu,
+  setSlideDownContent,
+}: {
+  item: MenuItemType;
+  sectionedMenu: string;
+  setSlideDownContent: Dispatch<SetStateAction<React.ReactNode | null>>;
+}) => {
+  const fieldRefs = useMenuItemConfig();
   const didRun = useRef(false);
 
   useEffect(() => {
     if (didRun.current) return; // Prevent running twice
     didRun.current = true;
     item.requiredOptions.forEach((data, idx) => {
-      mainEdit.addRequiredGroup(data.title);
+      fieldRefs.addRequiredGroup(data.title);
       data.choices.forEach((choice) =>
-        mainEdit.addRequiredItem(idx, choice.name, choice.price)
+        fieldRefs.addRequiredItem(idx, choice.name, choice.price)
       );
     });
     item.additionalOptions.forEach((option) =>
-      mainEdit.addAdditionalItem(option.name, option.price)
+      fieldRefs.addAdditionalItem(option.name, option.price)
     );
   }, []);
 
-  const applyChanges = () => {
-    console.log(mainEdit);
+  const applyChanges = async () => {
+    const nameField = fieldRefs.nameRef.current?.value;
+    const descriptionField = fieldRefs.descriptionRef.current?.value;
+    const priceField = Number(fieldRefs.priceRef.current?.value);
+    const requiredField = fieldRefs.requiredOptions.map((option) => {
+      const title = option.title?.current?.value;
+      const choices = option.choices.map((choice) => {
+        return {
+          name: choice.name.current?.value,
+          price: Number(choice.price.current?.value),
+        };
+      });
+      return { title: title, choices: choices };
+    });
+    const additionalField = fieldRefs.additionalChoices.map((choice) => {
+      return {
+        name: choice.name.current?.value,
+        price: Number(choice.price.current?.value),
+      };
+    });
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/menu/update/${sectionedMenu}/${item._id}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: nameField,
+          description: descriptionField,
+          price: priceField,
+          requiredOptions: requiredField,
+          additionalOptions: additionalField,
+        }),
+      }
+    );
+
+    if (response.ok) setSlideDownContent(null);
   };
 
   const loadRequiredOptions = useMemo(() => {
@@ -32,13 +77,13 @@ const MenuItemEdit = ({ item }: { item: MenuItemType }) => {
           <p className="text-2xl">Required Options</p>
           <button
             className="border border-gray-500 py-1 px-3"
-            onClick={() => mainEdit.addRequiredGroup("")}
+            onClick={() => fieldRefs.addRequiredGroup("")}
           >
             +
           </button>
         </div>
 
-        {mainEdit.requiredOptions.map((data, idx) => {
+        {fieldRefs.requiredOptions.map((data, idx) => {
           return (
             <div key={data.id}>
               <div className="flex gap-4 flex-row items-center justify-center">
@@ -53,24 +98,24 @@ const MenuItemEdit = ({ item }: { item: MenuItemType }) => {
               <InputGroups
                 data={data.choices}
                 removeItem={(itemId) =>
-                  mainEdit.removeRequiredItem(idx, itemId)
+                  fieldRefs.removeRequiredItem(idx, itemId)
                 }
               />
               <div className="flex justify-center gap-4 mt-2">
                 <button
                   className="bg-green-500 p-2"
-                  onClick={() => mainEdit.addRequiredItem(idx, "", -1)}
+                  onClick={() => fieldRefs.addRequiredItem(idx, "", -1)}
                 >
                   Add Item
                 </button>
                 <button
                   className="bg-red-500 p-2"
-                  onClick={() => mainEdit.removeRequiredGroup(data.id)}
+                  onClick={() => fieldRefs.removeRequiredGroup(data.id)}
                 >
                   Remove Group
                 </button>
               </div>
-              {idx !== mainEdit.requiredOptions.length - 1 && (
+              {idx !== fieldRefs.requiredOptions.length - 1 && (
                 <hr className="my-3" />
               )}
             </div>
@@ -78,7 +123,7 @@ const MenuItemEdit = ({ item }: { item: MenuItemType }) => {
         })}
       </div>
     );
-  }, [mainEdit.requiredOptions]);
+  }, [fieldRefs.requiredOptions]);
 
   const loadAdditionalChoices = useMemo(() => {
     return (
@@ -87,18 +132,18 @@ const MenuItemEdit = ({ item }: { item: MenuItemType }) => {
           <p className="text-2xl">Additional Choices</p>
           <button
             className="border border-gray-500 py-1 px-3"
-            onClick={() => mainEdit.addAdditionalItem("", -1)}
+            onClick={() => fieldRefs.addAdditionalItem("", -1)}
           >
             +
           </button>
         </div>
         <InputGroups
-          data={mainEdit.additionalChoices}
-          removeItem={(id) => mainEdit.removeAdditionalItem(id)}
+          data={fieldRefs.additionalChoices}
+          removeItem={(id) => fieldRefs.removeAdditionalItem(id)}
         />
       </div>
     );
-  }, [mainEdit.additionalChoices]);
+  }, [fieldRefs.additionalChoices]);
 
   return (
     <div className="bg-inherit">
@@ -109,7 +154,7 @@ const MenuItemEdit = ({ item }: { item: MenuItemType }) => {
       <div className="flex flex-col items-center p-4 border border-x-0 border-t-0 border-white">
         <div className="text-2xl">Name</div>
         <input
-          ref={mainEdit.nameRef}
+          ref={fieldRefs.nameRef}
           type="text"
           className="text-base my-2 bg-transparent border border-gray-500 p-2 resize-none text-white w-full outline-none"
           defaultValue={item.name}
@@ -118,7 +163,7 @@ const MenuItemEdit = ({ item }: { item: MenuItemType }) => {
       <div className="flex flex-col items-center p-4 border border-x-0 border-t-0 border-white">
         <div className="text-2xl">Description</div>
         <input
-          ref={mainEdit.descriptionRef}
+          ref={fieldRefs.descriptionRef}
           type="text"
           className="text-base my-2 bg-transparent border border-gray-500 p-2 resize-none text-white w-full outline-none"
           defaultValue={item.description}
@@ -127,7 +172,7 @@ const MenuItemEdit = ({ item }: { item: MenuItemType }) => {
       <div className="flex flex-col items-center p-4 border border-x-0 border-t-0 border-white">
         <div className="text-2xl">Price</div>
         <input
-          ref={mainEdit.priceRef}
+          ref={fieldRefs.priceRef}
           type="number"
           className="text-base my-2 bg-transparent border border-gray-500 p-2 resize-none text-white w-full outline-none"
           inputMode="numeric"
