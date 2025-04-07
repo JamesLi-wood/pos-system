c9d270c309f537eb02d58a083448cf19276265d0
james <jamesliwood@gmail.com>
2025-04-07 01:31:52 -0400

    opted for react context instead of sending down props

--- frontend/app/dashboard/menu/menu.tsx
+++ frontend/app/dashboard/menu/menu.tsx
@@ -1,24 +1,37 @@
-import { useState } from "react";
+import { useState, createContext } from "react";
 import SectionedMenu from "./sectionedMenu";
 import MenuItems from "./menuItems";
 import { MenuItemType } from "../../types";
 import useFetchRestaurantData from "@/app/hooks/useFetchRestaurantData";
+import { dashboardMenuContextType } from "../../types";
+
+export const menuContext = createContext<dashboardMenuContextType | undefined>(
+  undefined
+);
 
 const Menu = () => {
   const { menu, refetchData } = useFetchRestaurantData();
   const [menuItems, setMenuItems] = useState<MenuItemType[] | null>(null);
   const [sectionedMenu, setSectionedMenu] = useState("");
+  const [slideDownContent, setSlideDownContent] =
+    useState<React.ReactNode | null>(null);
+
+  const contextValue = {
+    refetchData,
+    setMenuItems,
+    sectionedMenu,
+    setSectionedMenu,
+    slideDownContent,
+    setSlideDownContent,
+  };
 
   return (
-    <div className="flex">
-      <SectionedMenu
-        menu={menu}
-        setMenuItems={setMenuItems}
-        setSectionedMenu={setSectionedMenu}
-        refetchData={refetchData}
-      />
+    <div className="flex max-md:flex-row flex-col border w-full">
+      <menuContext.Provider value={contextValue}>
+        <SectionedMenu menu={menu} />
 
-      <MenuItems items={menuItems} sectionedMenu={sectionedMenu} />
+        <MenuItems items={menuItems} />
+      </menuContext.Provider>
     </div>
   );
 };
