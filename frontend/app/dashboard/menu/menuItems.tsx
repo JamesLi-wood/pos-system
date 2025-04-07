b2d3b30050f946d60de32dbb5563bbbd35e03709
james <jamesliwood@gmail.com>
2025-04-07 01:34:03 -0400

    received prop variables through useContext and updated some css

--- frontend/app/dashboard/menu/menuItems.tsx
+++ frontend/app/dashboard/menu/menuItems.tsx
@@ -1,69 +1,51 @@
-import { useEffect, useState } from "react";
-import { MenuItemType } from "@/app/types";
-import SlideDown from "@/app/components/slidedown";
+import { useContext } from "react";
 import MenuItemAdd from "./menuItemAdd";
 import MenuItemEdit from "./menuItemEdit";
+import SlideDown from "@/app/components/slidedown";
+import { menuContext } from "./menu";
+import { MenuItemType } from "@/app/types";
 
-const MenuItems = ({
-  items,
-  sectionedMenu,
-}: {
-  items: MenuItemType[] | null;
-  sectionedMenu: string;
-}) => {
-  const [showEdit, setShowEdit] = useState(-1);
-  const [slideDownContent, setSlideDownContent] =
-    useState<React.ReactNode | null>(null);
-
-  useEffect(() => {
-    setShowEdit(-1);
-  }, [items]);
+const MenuItems = ({ items }: { items: MenuItemType[] | null }) => {
+  const context = useContext(menuContext);
+  if (!context) {
+    throw new Error("tableContext must be used within a Provider");
+  }
+  const { slideDownContent, setSlideDownContent } = context;
 
   return (
-    <div>
+    <div className="overflow-y-scroll">
       {items && (
-        <>
-          {items.map((item, idx) => {
+        <div className="flex flex-row flex-wrap justify-center">
+          {items.map((item) => {
             return (
-              <div key={item._id} className="flex">
-                <p
-                  onClick={() => {
-                    showEdit === idx ? setShowEdit(-1) : setShowEdit(idx);
-                  }}
-                >
-                  {item.name}
-                </p>
-                {showEdit === idx && (
-                  <button
-                    onClick={() => {
-                      setSlideDownContent(
-                        <MenuItemEdit
-                          item={item}
-                          sectionedMenu={sectionedMenu}
-                          setSlideDownContent={setSlideDownContent}
-                        />
-                      );
-                    }}
-                  >
-                    Edit
-                  </button>
-                )}
+              <div key={item._id} className="flex flex-col border w-96">
+                <div className="h-32 border">IMAGE</div>
+                <div className="flex flex-row justify-between items-center px-4">
+                  <p>{item.name}</p>
+
+                  <div className="flex gap-4 border">
+                    <button
+                      className="bg-blue-500 p-2"
+                      onClick={() => {
+                        setSlideDownContent(<MenuItemEdit item={item} />);
+                      }}
+                    >
+                      Edit
+                    </button>
+                    <button className="bg-red-500 p-2">Delete</button>
+                  </div>
+                </div>
               </div>
             );
           })}
           <button
             onClick={() => {
-              setSlideDownContent(
-                <MenuItemAdd
-                  sectionedMenu={sectionedMenu}
-                  setSlideDownContent={setSlideDownContent}
-                />
-              );
+              setSlideDownContent(<MenuItemAdd />);
             }}
           >
             Add
           </button>
-        </>
+        </div>
       )}
 
       {slideDownContent && (
