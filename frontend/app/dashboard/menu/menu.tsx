e9807066dd43288bd395ac7cbc820b404033b19d
james <jamesliwood@gmail.com>
2025-04-23 02:09:09 -0400

    moved slideDownContent logic here and added a useEffect to change the menuItems when menu is updated

--- frontend/app/dashboard/menu/menu.tsx
+++ frontend/app/dashboard/menu/menu.tsx
@@ -1,16 +1,16 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
+import SlideDown from "@/app/components/slidedown";
 import SectionedMenu from "./sectionedMenu";
 import MenuItems from "./menuItems";
-import { MenuItemType } from "../../types";
 import useFetchRestaurantData from "@/app/hooks/useFetchRestaurantData";
-import { dashboardMenuContextType } from "../../types";
+import { dashboardMenuContextType, MenuItemType } from "../../types";
 
 export const menuContext = createContext<dashboardMenuContextType | undefined>(
   undefined
 );
 
 const Menu = () => {
-  const { menu, refetchData } = useFetchRestaurantData();
+  const { menu, refetchData} = useFetchRestaurantData();
   const [menuItems, setMenuItems] = useState<MenuItemType[] | null>(null);
   const [sectionedMenu, setSectionedMenu] = useState("");
   const [slideDownContent, setSlideDownContent] =
@@ -21,16 +21,31 @@ const Menu = () => {
     setMenuItems,
     sectionedMenu,
     setSectionedMenu,
-    slideDownContent,
     setSlideDownContent,
   };
 
+  useEffect(() => {
+    if (sectionedMenu) {
+      const data = menu.find((item) => item.name == sectionedMenu)?.data;
+      if (data) setMenuItems(data);
+    }
+  }, [menu]);
+
   return (
-    <div className="flex max-md:flex-row flex-col border w-full">
+    <div className="flex flex-col border w-full">
       <menuContext.Provider value={contextValue}>
         <SectionedMenu menu={menu} />
-
         <MenuItems items={menuItems} />
+
+        {slideDownContent && (
+          <SlideDown
+            handleRemove={() => {
+              setSlideDownContent(null);
+            }}
+          >
+            {slideDownContent}
+          </SlideDown>
+        )}
       </menuContext.Provider>
     </div>
   );
