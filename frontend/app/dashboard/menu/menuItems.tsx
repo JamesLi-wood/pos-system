2f351f1e10616e977f66647234fb5a1bc797241d
james <jamesliwood@gmail.com>
2025-04-28 04:15:04 -0400

    updated css and replaced text buttons with icons instead

--- frontend/app/dashboard/menu/menuItems.tsx
+++ frontend/app/dashboard/menu/menuItems.tsx
@@ -3,6 +3,9 @@ import MenuItemAdd from "./menuItemAdd";
 import MenuItemEdit from "./menuItemEdit";
 import { menuContext } from "./menu";
 import { HiOutlineBan } from "react-icons/hi";
+import { CgAddR } from "react-icons/cg";
+import { FaTrashAlt } from "react-icons/fa";
+import { RiSettings4Fill } from "react-icons/ri";
 import { MenuItemType } from "@/app/types";
 
 const MenuItems = ({ items }: { items: MenuItemType[] | null }) => {
@@ -56,24 +59,24 @@ const MenuItems = ({ items }: { items: MenuItemType[] | null }) => {
   };
 
   return (
-    <div className="overflow-y-scroll">
-      <div className="flex ml-4 mb-4 gap-4">
-        <p className="text-4xl">{sectionedMenu}</p>
+    <div className="overflow-y-scroll scroll-hidden mt-20">
+      <div className="flex ml-10 gap-4 items-center">
+        <p className="text-4xl max-sm:text-2xl">{sectionedMenu}</p>
         <button
-          className="bg-green-500 px-4 py-2"
           onClick={() => {
             setSlideDownContent(<MenuItemAdd />);
           }}
         >
-          Add Menu Item
+          <CgAddR className="w-10 h-10" />
         </button>
-        <button className="bg-red-500 px-4 py-2" onClick={deleteSectionedMenu}>
-          Delete
+
+        <button onClick={deleteSectionedMenu}>
+          <FaTrashAlt className="w-10 h-10" />
         </button>
       </div>
 
       {items && (
-        <div className="flex flex-row flex-wrap gap-4 justify-center">
+        <div className="my-8 mx-10 flex flex-wrap gap-4 justify-center">
           {items.map((item) => {
             const imageUrl = item.image.data
               ? `data:${item.image.contentType};base64,${item.image.data}`
@@ -92,23 +95,23 @@ const MenuItems = ({ items }: { items: MenuItemType[] | null }) => {
                     <HiOutlineBan className="w-full h-full" />
                   )}
                 </div>
-                <div className="flex flex-row justify-between items-center px-4">
+                <div className="flex flex-row justify-between items-center p-4">
                   <p>{item.name}</p>
 
-                  <div className="flex gap-4 border">
+                  <div className="flex gap-4 ">
                     <button
                       className="bg-blue-500 p-2"
                       onClick={() => {
                         setSlideDownContent(<MenuItemEdit item={item} />);
                       }}
                     >
-                      Edit
+                      <RiSettings4Fill className="w-7 h-7" />
                     </button>
                     <button
                       className="bg-red-500 p-2"
                       onClick={() => deleteMenuItem(item._id)}
                     >
-                      Delete
+                      <FaTrashAlt className="w-7 h-7" />
                     </button>
                   </div>
                 </div>
