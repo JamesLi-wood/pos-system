70f19ac03c9b9724d1523750b89205f6422aa1ad
james <jamesliwood@gmail.com>
2025-05-01 02:39:06 -0400

    replaced tableDetail state with setSlidedown component

--- frontend/app/table/takeout.tsx
+++ frontend/app/table/takeout.tsx
@@ -1,12 +1,18 @@
 import { SetStateAction, useEffect, useState, useMemo } from "react";
 import { useSocket } from "../hooks/useSocket";
 import { TicketType } from "../types";
-import SlideDown from "../components/slidedown";
 import Item from "./item";
 
-const Takeout = ({ activateInventory }: { activateInventory: () => void }) => {
+const Takeout = ({
+  activateInventory,
+  setSlidedownContent,
+}: {
+  activateInventory: () => void;
+  setSlidedownContent: React.Dispatch<
+    React.SetStateAction<React.ReactNode | null>
+  >;
+}) => {
   const [takeoutTickets, setTakeoutTickets] = useState<TicketType[]>([]);
-  const [takeoutDetails, setTakeoutDetails] = useState<TicketType | null>(null);
   const socket = useSocket();
 
   useEffect(() => {
@@ -45,40 +51,28 @@ const Takeout = ({ activateInventory }: { activateInventory: () => void }) => {
     }
   };
 
-  const handleTakeoutTickets = useMemo(() => {
-    return takeoutTickets.map((data: TicketType) => {
-      return (
-        <div
-          key={data.orderID}
-          className="py-4 bg-white text-black mb-2 w-[90%] rounded-2xl"
-        >
-          <div className="flex flex-col items-start justify-between px-4">
-            <div>
-              <p>{`Order #${data.orderID}`}</p>
-              <p>{data.name}</p>
-              <p>{data.phoneNumber}</p>
-            </div>
-            <div className="flex mt-2 w-full gap-1 justify-center">
-              <button
-                className="cursor-pointer rounded border-none bg-blue-600 p-4 text-white"
-                onClick={() => {
-                  setTakeoutDetails(data);
-                }}
-              >
-                Details
-              </button>
-              <button
-                className="cursor-pointer rounded border-none bg-red-500 p-4 text-white"
-                onClick={() => removeOrder(data.orderID)}
-              >
-                Remove
-              </button>
-            </div>
-          </div>
+  const TakeoutDetail = ({ detail }: { detail: TicketType }) => {
+    return (
+      <div className="bg-inherit px-4">
+        <div className="sticky top-0 z-10 bg-inherit text-3xl">
+          <p className="py-4">{`Order# ${detail.orderID}`}</p>
+          <hr />
         </div>
-      );
-    });
-  }, [takeoutTickets]);
+
+        <div className="my-2 flex gap-2 text-lg">
+          <p>{detail.name}</p>
+          <p>{detail.phoneNumber}</p>
+        </div>
+        <hr />
+
+        <div className="text-lg">
+          {detail.ticket.map((ticket, idx) => {
+            return <Item key={idx} item={ticket} />;
+          })}
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div>
@@ -93,35 +87,41 @@ const Takeout = ({ activateInventory }: { activateInventory: () => void }) => {
           </button>
         </div>
 
-        <div className="flex items-center flex-col">{handleTakeoutTickets}</div>
+        <div className="flex items-center flex-col">
+          {takeoutTickets.map((data: TicketType) => {
+            return (
+              <div
+                key={data.orderID}
+                className="py-4 bg-white text-black mb-2 w-[90%] rounded-2xl"
+              >
+                <div className="flex flex-col items-start justify-between px-4">
+                  <div>
+                    <p>{`Order #${data.orderID}`}</p>
+                    <p>{data.name}</p>
+                    <p>{data.phoneNumber}</p>
+                  </div>
+                  <div className="flex mt-2 w-full gap-1 justify-center">
+                    <button
+                      className="cursor-pointer rounded border-none bg-blue-600 p-4 text-white"
+                      onClick={() => {
+                        setSlidedownContent(<TakeoutDetail detail={data} />);
+                      }}
+                    >
+                      Details
+                    </button>
+                    <button
+                      className="cursor-pointer rounded border-none bg-red-500 p-4 text-white"
+                      onClick={() => removeOrder(data.orderID)}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-
-      {takeoutDetails && (
-        <SlideDown
-          handleRemove={() => {
-            setTakeoutDetails(null);
-          }}
-        >
-          <div className="bg-inherit px-4">
-            <div className="sticky top-0 z-10 bg-inherit text-3xl">
-              <p className="py-4">{`Order# ${takeoutDetails.orderID}`}</p>
-              <hr />
-            </div>
-
-            <div className="my-2 flex gap-2 text-lg">
-              <p>{takeoutDetails.name}</p>
-              <p>{takeoutDetails.phoneNumber}</p>
-            </div>
-            <hr />
-
-            <div className="text-lg">
-              {takeoutDetails.ticket.map((ticket, idx) => {
-                return <Item key={idx} item={ticket} />;
-              })}
-            </div>
-          </div>
-        </SlideDown>
-      )}
     </div>
   );
 };
