9a3c45353a88e728dbc555d15d6e6eb1f963eb8f
james <jamesliwood@gmail.com>
2026-01-15 05:40:05 -0500

    removed useMemo

--- frontend/app/kitchen/kitchenTicket.tsx
+++ frontend/app/kitchen/kitchenTicket.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useMemo } from "react";
 import useTimer from "../hooks/useTimer";
 import { TicketType } from "../types";
 
@@ -12,28 +11,6 @@ const KitchenTicket = ({
 }) => {
   const [formatTime] = useTimer(data.dateCreated);
 
-  const ticketMap = useMemo(() => {
-    return data.ticket.map((order, idx) => {
-      const requiredOptions = order.requiredOptions;
-      const additionalOptions = order.additionalOptions;
-
-      return (
-        <div key={idx} className="px-4 py-1">
-          <p>{`${order.quantity} ${order.name}`}</p>
-          {requiredOptions.length > 0 && (
-            <p className="ml-4">{`- ${requiredOptions.join(", ")}`}</p>
-          )}
-          {additionalOptions.length > 0 && (
-            <p className="ml-4">{`- ${additionalOptions.join(", ")}`}</p>
-          )}
-          {order.specialRequests && (
-            <p className="ml-4">{`- ${order.specialRequests}`}</p>
-          )}
-        </div>
-      );
-    });
-  }, []);
-
   return (
     <div className="flex flex-col justify-between bg-white">
       <div className="bg-[rgb(50,205,50)] flex flex-row justify-between px-4 py-2 text-white">
@@ -41,7 +18,27 @@ const KitchenTicket = ({
         <p>{formatTime()}</p>
       </div>
 
-      <div className="h-full py-2">{ticketMap}</div>
+      <div className="h-full py-2">
+        {data.ticket.map((order, idx) => {
+          const requiredOptions = order.requiredOptions;
+          const additionalOptions = order.additionalOptions;
+
+          return (
+            <div key={idx} className="px-4 py-1">
+              <p>{`${order.quantity} ${order.name}`}</p>
+              {requiredOptions.length > 0 && (
+                <p className="ml-4">{`- ${requiredOptions.join(", ")}`}</p>
+              )}
+              {additionalOptions.length > 0 && (
+                <p className="ml-4">{`- ${additionalOptions.join(", ")}`}</p>
+              )}
+              {order.specialRequests && (
+                <p className="ml-4">{`- ${order.specialRequests}`}</p>
+              )}
+            </div>
+          );
+        })}
+      </div>
 
       <button
         className="bg-[rgb(50,205,50)] cursor-pointer px-4 py-2 text-center text-white"
